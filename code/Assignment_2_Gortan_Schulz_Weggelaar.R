a9dc4270ffff71d9f744f90f6a6808d75257180a
felixschulz385 <felix@zoelch-schulz.de>
2025-04-13 17:07:14 +0200

    Add code for second assignment

--- code/Assignment_2_Gortan_Schulz_Weggelaar.R
+++ code/Assignment_2_Gortan_Schulz_Weggelaar.R
@@ -27,6 +27,7 @@ lapply(packaged_vector_did, require, character.only = TRUE)
 # --------------------------------------------------------------
 
 attach(data)
+data <- tibble(data)
 
 
 # question (a) (code is copy-pasted from lab session)
@@ -80,24 +81,98 @@ colnames(diff_output)<- c("E(Y|D=0, T=0)", "E(Y|D=1, T=0)",
                           "Difference", "s.e.", "p-value")
 print(round(diff_output, digits=2))
 
+## Felix' solution
+# following slide 11 of set 3
 
-# I think the estimates are the correct ones but I'm not use how to do the boostrapping here
+calculate_atet <- function(data, outcome_var) {
+  mean_d0_before <- mean(data[data$connected == 0 & data$submarines == 0, ][[outcome_var]], na.rm = TRUE)
+  mean_d1_before <- mean(data[data$connected == 1 & data$submarines == 0, ][[outcome_var]], na.rm = TRUE)
+  mean_d0_after <- mean(data[data$connected == 0 & data$submarines == 1, ][[outcome_var]], na.rm = TRUE)
+  mean_d1_after <- mean(data[data$connected == 1 & data$submarines == 1, ][[outcome_var]], na.rm = TRUE)
+  
+  atet <- mean_d1_after - mean_d1_before - (mean_d0_after - mean_d0_before)
+  return(atet)
+}
+
+# Example usage:
+atet <- calculate_atet(data, "employed")
+
+bootstrap.np <- function(data, outcome_var, n_iter){
+  obs <- nrow(data) # store the number of observations
+  atet_boot <- rep_len(NA, n_iter) # empty matrix for storing effect estimates 
+      
+  # The bootstrap loop starts here:
+  for(i in 1:n_iter){ 
+      
+    # draw a bootstrap sample    
+    sboot<-sample(x=1:obs, # observations from which to choose 
+                  size=obs, # number of obs as in original data
+                  replace=TRUE) # with replacement (one obs allowed to appear more than once)
+  
+    # redefine y, d, m and x from the bootstrap sample    
+    atet_boot[i] <- calculate_atet(data[sboot, ], outcome_var)
+
+  }
+
+  output <- list(sd=sd(atet_boot), distr=atet_boot)
+  return(output)
+
+}
+
+# Set seed for replicability 
+set.seed(1234)
+
+atet_sd <- bootstrap.np(data, "employed", 10)
+
+# Export in table format
+tibble(
+  atet = atet,
+  sd = atet_sd$sd,
+  lower_bound = atet - 1.96 * atet_sd$sd,
+  upper_bound = atet + 1.96 * atet_sd$sd
+) %>%
+  kableExtra::kbl(
+    "latex", booktabs = TRUE, 
+    caption = "Internet access and employment: Non-parametric bootstrap estimates of ATET",
+    label = "tab:np_atet",
+    ) %>%
+  kableExtra::kable_styling(latex_options = c("hold_position")) %>%
+  writeLines("output/tables/2_np_atet.tex")
+
+#--------------------------------------------------------
 
 # question (b) Parametric ATET, no fixed effects, no clustered SE
 did1 <- feols(employed ~ connected + submarines + treatment, data)
-did1 <- summary(did1)
-did1
+vcov_did1 <- vcov(did1)
 
 # question (c) Parametric ATET, no fixed effects, clustered SE
 did2 <- feols(employed ~ connected + submarines + treatment, data)
-did2 <- summary(did2, cluster = "location")
-did2
+vcov_did2 <- vcovCL(did2, cluster = ~ location)
 
 # question (d) Parametric ATET, with fixed effects,clustered SE
 did3 <- feols(employed ~ treatment | time + location, data)
-did3 <- summary(did3, cluster = "location")
-did3
+vcov_did3 <- vcovCL(did3, cluster = ~ location)
 
+# combine all in one table
+
+texreg::texreg(list(did1, did2, did3), 
+                override.se = list(sqrt(diag(vcov_did1)),
+                                   sqrt(diag(vcov_did2)),
+                                   sqrt(diag(vcov_did3))),
+                override.pvalues = list(2*pnorm(-abs(coef(did1)/sqrt(diag(vcov(did1))))),
+                                        2*pnorm(-abs(coef(did2)/sqrt(diag(vcov_did2)))),
+                                        2*pnorm(-abs(coef(did3)/sqrt(diag(vcov_did3))))),
+                custom.coef.names = c("Intercept", "Connected", "Submarines", "Treatment"),
+                custom.model.names = c("No fixed effects, no clustered SE", 
+                                       "No fixed effects, clustered SE",
+                                       "Fixed effects, clustered SE"),
+                caption = "Parametric ATET estimates",
+                caption.above = TRUE,
+                stars = c(0.001, 0.01, 0.05),
+                single.row = TRUE,
+                digits = 3,
+                file = "output/tables/2_parametric_atet_combined.tex",
+                )
 
 # --------------------------------------------------------------
 # Question 2: Adding Skilled to the regression
@@ -105,16 +180,95 @@ did3
 
 # question (b)
 did4 <- feols(employed ~ treatment + skilled | time + location, data)
-did4 <- summary(did4, cluster = "location")
-did4
+vcov_did4 <- vcovCL(did4, cluster = ~ location)
+
+texreg::texreg(list(did4), 
+                custom.coef.names = c("Treatment", "Skilled"),
+                custom.model.names = c("Fixed effects, clustered SE"),
+                custom.model.numbers = c("1"),
+                caption = "Parametric ATET, with fixed effects, controlling ofr skill and clustered SE",
+                caption.above = TRUE,
+                stars = c(0.001, 0.01, 0.05),
+                single.row = TRUE,
+                file = "output/tables/2_parametric_atet_skilled.tex",
+                vcov = vcov_did4)
+
+# --------------------------------------------------------------
+# Question 3: Fast Internet and Education
+# --------------------------------------------------------------
+
+# question (a): common trends plot
+
+# Create a group-means data set
+common_trends <- data %>% 
+                  group_by(time, connected) %>% 
+                  summarise(educ_high_mean = mean(educ_high)) %>%
+                  ungroup() %>%
+                  mutate(connected = recode_factor(connected, 
+                                            `0` = "Connected = 0",
+                                            `1` = "Connected = 1"))
+
+# Plot
+ggplot(data = common_trends, 
+       aes(x = time, y = educ_high_mean, 
+           group = connected, 
+           color = connected)) + 
+  geom_line() +
+  geom_vline(xintercept = 0.5, linetype="dashed") +
+  scale_x_continuous(breaks = seq(-5, 4, by = 1)) +
+  theme_bw(base_size = 20) +
+  labs( y ="Share of highly educated", x= "Time", colour = "Group")
+
+ggsave("output/figures/2_common_trends.png", width = 10, height = 6, dpi = 300)
+
+# question (b): event study plot
+
+event_study <- feols(educ_high ~ i(time, connected, 0) | time + location, data)
+
+iplot(event_study) 
+ggsave("output/figures/2_event_study.png", width = 10, height = 6, dpi = 300)
+
+# --------------------------------------------------------------
+# Question 4: Clustered Bootstrap SEs
+# --------------------------------------------------------------
+
+bootstrap.cluster.np <- function(data, outcome_var, cluster_var, n_iter){
+  clusters <- unique(data[[cluster_var]])
+  atet_boot <- rep(NA, n_iter) # vector for storing effect estimates 
+      
+  # Bootstrap loop
+  for(i in 1:n_iter){ 
+      
+    # draw a bootstrap sample of clusters
+    sboot <- sample(x = clusters, 
+                    size = length(clusters), 
+                    replace = TRUE)
+
+    # get data for the sampled clusters (with duplicates)
+    data_iter <- purrr::map_df(sboot, ~ data[data[[cluster_var]] == ., ])
+    
+    # compute nonparametric ATET on the bootstrap sample
+    atet_boot[i] <- calculate_atet(data_iter, outcome_var)
+  }
+
+  output <- list(sd = sd(atet_boot, na.rm = TRUE), distr = atet_boot)
+  return(output)
+}
 
 
+atet_sd <- bootstrap.cluster.np(data, "employed", "location", 10)
 
-
-
-
-
-
-
-
-
+# Export in table format
+tibble(
+  atet = atet,
+  sd = atet_sd$sd,
+  lower_bound = atet - 1.96 * atet_sd$sd,
+  upper_bound = atet + 1.96 * atet_sd$sd
+) %>%
+  kableExtra::kbl(
+    "latex", booktabs = TRUE, 
+    caption = "Internet access and employment: Non-parametric bootstrap estimates of ATET, Cluster-robust SEs",
+    label = "tab:np_atet_clustered",
+    ) %>%
+  kableExtra::kable_styling(latex_options = c("hold_position")) %>%
+  writeLines("output/tables/2_np_atet_cluster.tex")
