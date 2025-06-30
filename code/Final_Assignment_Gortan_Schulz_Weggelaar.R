5f7eb8b1222c230a056ed5d59d6270ec50648155
gortanm <marco.gortan@unibas.ch>
2025-06-30 11:52:41 +0200

    R file for big assignment

---
+++ code/Final_Assignment_Gortan_Schulz_Weggelaar.R
@@ -0,0 +1,11 @@
+# Lab Final Assignment – Causal Inference for Policy Evaluation
+# Spring Semester 2025
+# Due: 15.07.2025, 23:59
+# Authors: Marco Gortan, Felix Schulz, Benjamin Weggelaar
+
+# --------------------------------------------------------------
+# Setup
+# --------------------------------------------------------------
+
+# Load the data
+rm(list = ls())
